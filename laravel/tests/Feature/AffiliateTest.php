744f2aa2dd5f1594593971ddc10d62c54c8220e6
Paul Caligari <pcaligari68@gmail.com>
2024-07-28 09:48:11 +0100

    chosed to look for a specific affiliate - it's a bad test but lets us push on quickly and we will use this as a springboard to get into the meat of the problem.  Tests will get refactored as we go to improve things

--- laravel/tests/Feature/AffiliateTest.php
+++ laravel/tests/Feature/AffiliateTest.php
@@ -24,11 +24,14 @@ class AffiliateTest extends TestCase
     public function test_the_invites_table_is_present(TestResponse $response) :void
     {
         // Not the best test but it'll do to demonstrate the principle..
+        // Looking for an affiliate we know should be in the list to force us to
+        // add a list to the view - crappy but lets us make progress quickly
         $response->assertSee([
             'Affiliate Id',
             'Name',
             'Lat',
-            'Long'
+            'Long',
+            'Yosef Giles'
         ]);
     }
 }
