7948886e61d42223343af71d6873718dd52ba17d
Paul Caligari <pcaligari68@gmail.com>
2024-07-28 07:35:46 +0000

    Add first failing test case - do we have a route

---
+++ laravel/tests/Feature/AffiliateTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AffiliateTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/invites');
+
+        $response->assertStatus(200);
+    }
+}
