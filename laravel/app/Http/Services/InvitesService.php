7dfce2f8badd1833ceb9940d5318cda09c26e54c
Paul Caligari <pcaligari68@gmail.com>
2024-07-28 11:25:03 +0100

    create the service class

---
+++ laravel/app/Http/Services/InvitesService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Services;
+
+class InvitesService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
