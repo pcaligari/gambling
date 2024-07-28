f75e4391f94aa0e38dc82d6331c2ecccb377ef36
Paul Caligari <pcaligari68@gmail.com>
2024-07-28 09:17:01 +0100

    add test for contents - this will force creation of our blade

--- laravel/tests/Feature/AffiliateTest.php
+++ laravel/tests/Feature/AffiliateTest.php
@@ -1,7 +1,8 @@
 <?php
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\TestResponse;
+use PHPUnit\Framework\Attributes\Depends;
 use Tests\TestCase;
 
 class AffiliateTest extends TestCase
@@ -9,10 +10,25 @@ class AffiliateTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_invites_can_be_rendered(): TestResponse
     {
         $response = $this->get('/invites');
 
         $response->assertStatus(200);
+
+        return $response;
+    }
+
+
+     #[Depends('test_the_invites_can_be_rendered')]
+    public function test_the_invites_table_is_present(TestResponse $response) :void
+    {
+        // Not the best test but it'll do to demonstrate the principle..
+        $response->assertSee([
+            'Affiliate Id',
+            'Name',
+            'Lat',
+            'Long'
+        ]);
     }
 }
